1962997185c690422386ca414a358fa6a9f7155f
Geoffrey Ian Ward <geoffreyianward@Geoffreys-MacBook-Pro.local>
2019-10-01 15:26:51 -0500

    more express routing

--- index.js
+++ index.js
@@ -8,11 +8,34 @@ const moment = require('moment');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// serve static files
+app.use(express.static(`${__dirname}/../client/dist`));
 
 
 
 
 
+
+
+
+
+
+// force requests to client files
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
+});
+
+const port = process.env.port || 3000;
 
 
 app.listen(3000, () =>
